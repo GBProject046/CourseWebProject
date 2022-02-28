49e83922e5b7f7b24794b1b3401e0faf6387ef33
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-02-28 12:25:28 -0300

    MySQL adaptation and first migration

--- CourseWebProject/Startup.cs
+++ CourseWebProject/Startup.cs
@@ -37,7 +37,8 @@ namespace CourseWebProject
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CourseWebProjectContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CourseWebProjectContext")));
+                    options.UseMySql(Configuration.GetConnectionString("CourseWebProjectContext"), builder =>
+                        builder.MigrationsAssembly("CourseWebProject")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

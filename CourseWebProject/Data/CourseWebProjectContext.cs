8a5f2be98c7af7ca2c09773f0d1c5cd743dea013
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-02-28 11:54:49 -0300

    CRUD Scaffolding

---
+++ CourseWebProject/Data/CourseWebProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CourseWebProject.Models;
+
+namespace CourseWebProject.Data
+{
+    public class CourseWebProjectContext : DbContext
+    {
+        public CourseWebProjectContext (DbContextOptions<CourseWebProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CourseWebProject.Models.Department> Department { get; set; }
+    }
+}
--- CourseWebProject/Startup.cs
+++ CourseWebProject/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CourseWebProject.Data;
 
 namespace CourseWebProject
 {
@@ -33,6 +35,9 @@ namespace CourseWebProject
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CourseWebProjectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CourseWebProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

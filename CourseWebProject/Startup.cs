913976772e89a2e217782148e96649a758e0dba7
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-03-01 11:49:36 -0300

    Seeding Service

--- CourseWebProject/Startup.cs
+++ CourseWebProject/Startup.cs
@@ -39,14 +39,17 @@ namespace CourseWebProject
             services.AddDbContext<CourseWebProjectContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("CourseWebProjectContext"), builder =>
                         builder.MigrationsAssembly("CourseWebProject")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {

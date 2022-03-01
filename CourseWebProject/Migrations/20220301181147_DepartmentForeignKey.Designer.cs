85f247e6f10225de95902a01e14de25ca707eab7
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-03-01 15:15:38 -0300

    Foreign key not null (referential integrity)

--- CourseWebProject/Migrations/20220301181147_DepartmentForeignKey.Designer.cs
+++ CourseWebProject/Migrations/20220301181147_DepartmentForeignKey.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CourseWebProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CourseWebProject.Migrations
 {
     [DbContext(typeof(CourseWebProjectContext))]
-    partial class CourseWebProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20220301181147_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

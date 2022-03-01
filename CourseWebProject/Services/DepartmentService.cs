c5077247609d2a718276efab5963f36b988e9e03
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-03-01 15:44:03 -0300

    SellerFormViewModel and Department select component

---
+++ CourseWebProject/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using CourseWebProject.Data;
+using CourseWebProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CourseWebProject.Services
+{
+    public class DepartmentService
+    {
+        private readonly CourseWebProjectContext _context;
+
+        public DepartmentService(CourseWebProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+
+    }
+}

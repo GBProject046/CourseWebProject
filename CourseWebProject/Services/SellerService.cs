995522241a4bd573e6209c622f32b13ec21266cf
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-03-03 12:24:58 -0300

    Seller details and eager loading

--- CourseWebProject/Services/SellerService.cs
+++ CourseWebProject/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using CourseWebProject.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseWebProject.Services
 {
@@ -27,7 +28,7 @@ namespace CourseWebProject.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         
         public void Remove(int id)

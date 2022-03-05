1a43af482161785ff72d3c41c58947ff585222ec
Gabriel Silva <gabriel.vcsilva@outlook.com>
2022-03-05 15:10:17 -0300

    Exception handling for delete (referential integrity)

--- CourseWebProject/Services/SellerService.cs
+++ CourseWebProject/Services/SellerService.cs
@@ -32,12 +32,20 @@ namespace CourseWebProject.Services
         {
             return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
         }
-        
+
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                
+                throw new IntegrityException(e.Message);
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
@@ -47,13 +55,13 @@ namespace CourseWebProject.Services
             {
                 throw new NotFoundException("Id not found!");
             }
-            
+
             try
             {
                 _context.Update(obj);
                 await _context.SaveChangesAsync();
             }
-            catch(DbUpdateConcurrencyException e)
+            catch (DbUpdateConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
             }
